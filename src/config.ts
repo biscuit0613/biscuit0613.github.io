8ea6fc5797dda25251957aff63efbe9a47afff7d
bhn <biscuit0613@qq.com>
2025-08-30 23:43:46 +0800

    优化极限与连续部分的目录及行文逻辑

--- src/config.ts
+++ src/config.ts
@@ -67,7 +67,7 @@ export const siteConfig: SiteConfig = {
 		carousel: {
 			enable: true, // 为 true 时：为多张图片启用轮播。为 false 时：从数组中随机显示一张图片
 
-			interval: 1.5, // 轮播间隔时间（秒）
+			interval: 8, // 轮播间隔时间（秒）
 		},
 
 		homeText: {
@@ -85,7 +85,7 @@ export const siteConfig: SiteConfig = {
 
 				speed: 100, // 打字速度（毫秒）
 				deleteSpeed: 50, // 删除速度（毫秒）
-				pauseTime: 2000, // 完全显示后的暂停时间（毫秒）
+				pauseTime: 12000, // 完全显示后的暂停时间（毫秒）
 			},
 		},
 
